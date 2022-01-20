53e6b6a957a1348c94d09323ac196beab861e1e1
Mango <92181249+GitRaut@users.noreply.github.com>
2022-01-20 12:25:49 +0300

    Update itc_pos_neg_separator_lst.cpp

--- itc_pos_neg_separator_lst.cpp
+++ itc_pos_neg_separator_lst.cpp
@@ -8,10 +8,10 @@ void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vect
                 lst1.push_back(lst[i]);
             }
             else if(lst[i] > 0){
-                lst2.push_back(lst[i]);
+                lst3.push_back(lst[i]);
             }
             else{
-                lst3.push_back(lst[i]);
+                lst2.push_back(lst[i]);
             }
         }
     }
